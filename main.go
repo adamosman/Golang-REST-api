0fcf217d77145d047759990887bd8fd6ef2f8bcd
adamosman <adaosman@gmail.com>
2018-03-26 09:43:02 -0400

    added Node/React for front-end

--- main.go
+++ main.go
@@ -2,99 +2,31 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
-	"math/rand"
 	"net/http"
-	"strconv"
 	"strings"
-
-	"github.com/gorilla/mux"
 )
 
-// Book Struct (Model)
-type Book struct {
-	ID     string  `json:"id"`
-	Isbn   string  `json:"isbn"`
-	Title  string  `json:"title"`
-	Author *Author `json:"author"`
+// WordCountPair struct
+type WordCountPair struct {
+	Word  string `json:"word"`
+	Count int    `json:"count"`
 }
 
-// Author Struct
-type Author struct {
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
-}
-
-// Init books var as a slice Book struct
-var books []Book
-
-// Get all Books
-func getBooks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(books)
-}
-
-// Get Single Book
-func getBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r) // Get params
-	// Loop through books and find with ID
-	for _, item := range books {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(&Book{})
-}
-
-// Create a New Book
-func createBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var book Book // variable 'book' set to the Book struct
-	_ = json.NewDecoder(r.Body).Decode(&book)
-	book.ID = strconv.Itoa(rand.Intn(10000000)) // Mock ID - NOT SAFE
-	books = append(books, book)
-	json.NewEncoder(w).Encode(book)
-}
-
-// Update Book
-func updateBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range books {
-		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
-			var book Book // variable 'book' set to the Book struct
-			_ = json.NewDecoder(r.Body).Decode(&book)
-			book.ID = params["id"]
-			books = append(books, book)
-			json.NewEncoder(w).Encode(book)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(books)
-}
-
-// Delete Book
-func deleteBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range books {
-		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
-			break
-		}
-	}
-	json.NewEncoder(w).Encode(books)
+// ResponseBodyStruct struct
+type ResponseBodyStruct struct {
+	WordCountPairs []WordCountPair `json:"wordCountPairs"`
+	WordCount      int             `json:"wordCount"`
 }
 
 // Upload for files
 func Upload(w http.ResponseWriter, r *http.Request) {
 	var str string
 	counts := map[string]int{}
+
+	var countPairs []WordCountPair
+
 	if r.Method == http.MethodPost {
 		f, _, err := r.FormFile("usrfile")
 		if err != nil {
@@ -112,22 +44,32 @@ func Upload(w http.ResponseWriter, r *http.Request) {
 		}
 		str = string(bs)
 	}
+
 	for _, word := range strings.Fields(str) {
 		counts[word]++
 	}
-	json, _ := json.MarshalIndent(counts, "", " ")
-	words := strings.Fields(str)
-	w.Header().Set("CONTENT-TYPE", "text/html; charset=UTF-8")
-	fmt.Fprintf(w, `<form action="/" method="post" enctype="multipart/form-data">
-					Upload a file<br>
-					<input type="file" name="usrfile"><br>
-					<input type="submit">
-					</form>
-					<br>
-					<br>
-					<h1>Unique word count: %s</h1>
-					<br>
-					<h1>Total word count: %v</h1>`, json, len(words))
+
+	for word, count := range counts {
+		var toAppend WordCountPair
+		toAppend.Word = word
+		toAppend.Count = count
+		countPairs = append(countPairs, toAppend)
+	}
+
+	// countPairsJSON, _ := json.MarshalIndent(countPairs, "", " ")
+	wordCount := len(strings.Fields(str))
+
+	var responseBody ResponseBodyStruct
+	responseBody.WordCount = wordCount
+	responseBody.WordCountPairs = countPairs
+
+	w.Header().Set("CONTENT-TYPE", "application/json; charset=UTF-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	responseJSON, _ := json.MarshalIndent(responseBody, "", "")
+
+	// writeToDisk({})
+
+	w.Write(responseJSON)
 }
 
 // WordCount counts the frequency of each word
@@ -146,21 +88,7 @@ func TotalWordCount(s string) int {
 }
 
 func main() {
-	// Init Router
-	// r := mux.NewRouter()
-
-	// Mock Data - @todo implement DB
-	// books = append(books, Book{ID: "1", Isbn: "526421", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
-	// books = append(books, Book{ID: "2", Isbn: "364213", Title: "Book Two", Author: &Author{Firstname: "Steve", Lastname: "Smith"}})
-
-	// Route Handlers / Endpoints
-	// r.HandleFunc("/api/books", getBooks).Methods("GET")
-	// r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
-	// r.HandleFunc("/api/books", createBook).Methods("POST")
-	// r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
-	// r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
-
-	// log.Fatal(http.ListenAndServe(":8000", r))
 	http.HandleFunc("/", Upload)
+	log.Println("Listening on Port 8000")
 	http.ListenAndServe(":8000", nil)
 }
